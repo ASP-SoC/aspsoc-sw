7128c5e71901d33039dcb16e2dbf9451ad9455d6
Fx <FxSt@gmx.at>
2018-02-08 20:43:18 +0100

    add blinking leds

--- seven_seg_txt.c
+++ seven_seg_txt.c
@@ -13,6 +13,8 @@ int main(){
 	unsigned char text_led[10] = {0x77,0x6D,0x73,0x40,0x6D,0x5C,0x39,0x00,0x00,0x00};
 	unsigned int seven_seg1 = 0x39 | (0x5C<<7) | (0x6D<<14);
 	unsigned int seven_seg2 = (0x73) | (0x6D<<7) | (0x77<<14);
+	
+	unsigned int led_data = 0xAAA;
   
 	printf("run...\n");
 	
@@ -21,6 +23,9 @@ int main(){
 	
 	while(1){ // infinite loop
     usleep(SLEEP);
+	
+	led_data = ~led_data;
+    write_lW(&led_data,PIO_LEDS_BASE,1);
 
     rotate_display_hex(text_led,sizeof(text_led));
   }
